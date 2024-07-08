f2aaffda360fc228f873aafab0bbfab4ff864219
Lucas Jorge de Araujo Torres <ljat@windows.cin.ufpe.br>
2024-07-08 17:43:48 -0300

    refactor:client

--- second_step/client.py
+++ second_step/client.py
@@ -13,98 +13,75 @@ class UDPClient:
         self.client_socket.bind(socket_binding) # Binding the socket to the address
         self.state = ""
 
-    def sendAck(self, ack, seq, target_address):
-        # global endFlag
+    def send_acknowledgement(self, ack, sequence_number, target_address):
+        print('\x1b[1;34;40m' + f'ACK = {sequence_number} sent' + '\x1b[0m')
         data = struct.pack('i', ack)
-        # receiverSocket.sendto(data, addr)
-        if randint(0, 9):  # 10% loss rate
+        if randint(0, 9):  # Simulating 10% loss rate
             self.client_socket.sendto(data, target_address) # Sends the data to the destination
         else: 
-            print('\x1b[1;31;40m' + f'Ack {seq} lost' + '\x1b[0m')
-            # endFlag = 0
+            print('\x1b[1;31;40m' + f'ACK = {sequence_number} lost' + '\x1b[0m')
+        if sequence_number == 0:
+            self.state = "wait_seq_1" # Change state to wait_seq_1
+        elif sequence_number == 1:
+            self.state = "wait_seq_0" # Change state to wait_seq_1
+
+    def waiting_for_packet(self, file, expected_seq_number):
+        print(f"Waiting for a packet whose sequence_number = {expected_seq_number}")
+        try:
+            packet, target_address = self.client_socket.recvfrom(MAX_BUFF_SIZE) # Receives the packet if any arrives
+        except: 
+            print(f'an exception occurred (wait_seq_{expected_seq_number})')
+            pass # While there is no packet to receive, just wait
+        else:
+            print('\x1b[1;32;40m' + 'Packet received' + '\x1b[0m')
+            length = len(packet) - 4
+            packet = struct.unpack_from(f'i {length}s', packet)
+            sequence_number = packet[0]
+            payload = packet[1:]
+
+            if sequence_number == expected_seq_number:
+                if(payload[0] == b'END'): # If received final message from sender, end the loop
+                    self.file_completed = True
+                for i in payload:
+                    file.write(i)
+
+                self.action = f"send_ack_{expected_seq_number}"  # Upon receiving the packet, if the sequence number is zero or one, send the corresponding ack
+            else: 
+                if expected_seq_number == 0:
+                    self.action = "send_ack_1" # If the packet is not the correct one, send the corresponding ack instead
+                elif expected_seq_number == 1:
+                    self.action  = "send_ack_0" # If the packet is not the correct one, send the corresponding ack instead
+                if(payload[0] == b'END'): # If received final message from sender, end the loop
+                    self.file_completed = True    
 
     def receive(self):
 
-        self.endFlag = 0
+        self.file_completed = False # Flag to indicate that file was completely sent
 
-        message, target_address = self.client_socket.recvfrom(MAX_BUFF_SIZE)
+        print("Waiting for a packet whose sequence_number = 0")
+        message, target_address = self.client_socket.recvfrom(MAX_BUFF_SIZE) # Receiving the file name
         file_name = message.decode().replace('\0', '')
+        print('\x1b[1;32;40m' + 'Packet received' + '\x1b[0m')
 
         file_name = "received_" + file_name # Rename the file
         file = open(file_name, 'wb') # Open the file in binary mode
 
-        self.action = "sendAck0"
+        self.action = "send_ack_0"
 
-        print("exited")
-
-        while(True): # Main loop of the receiver's finite state machine
+        while not self.file_completed: # Main loop of the receiver's finite state machine
     
-            if self.state == "waitSeq_0": # State of waiting for packet 0
-                print("Waiting for SeqNum=0")
-                try:
-                    pckg, target_address = self.client_socket.recvfrom(MAX_BUFF_SIZE) # Receives the packet if any arrives
-                except: 
-                    print('an exception occurred (waitSeq_0)')
-                    pass # While there is no packet to receive, just wait
-                else:
-                    print('\x1b[1;32;40m' + 'Packet received' + '\x1b[0m')
-                    tam = len(pckg) - 4
-                    pckg = struct.unpack_from(f'i {tam}s', pckg)
-                    seq = pckg[0]
-                    payload = pckg[1:]
+            if self.state == "wait_seq_0": # State of waiting for packet 0
+                self.waiting_for_packet(file, 0)
 
-                    if seq == 0:
-                        if(payload[0] == b'END'): # If received final message from sender, end the loop
-                            self.endFlag = 1
-                        for i in payload:
-                            file.write(i)
+            elif self.state == "wait_seq_1": # State of waiting for packet 1
+                self.waiting_for_packet(file, 1)
 
-                        self.action = "sendAck0"  # Upon receiving the packet, if the sequence number is zero, send the corresponding ack
-                    else: 
-                        self.action = "sendAck1" # If the packet is not the correct one, send sequence 1 ack instead
-                        if(payload[0] == b'END'): # If received final message from sender, end the loop
-                            self.endFlag = 1
+            if self.action == "send_ack_0":
+                self.send_acknowledgement(0, 0, target_address) # Send sequence 0 ack    
+            elif self.action == "send_ack_1":
+                self.send_acknowledgement(1, 1, target_address) # Send sequence 1 ack
 
-            elif self.state == "waitSeq_1": # State of waiting for packet 1
-                print("Waiting for SeqNum=1")
-                try:
-                    pckg, target_address = self.client_socket.recvfrom(MAX_BUFF_SIZE) # Receives the packet if any arrives
-                except: 
-                    print('an exception occurred (waitSeq_1)')
-                    pass  # Wait until a packet arrives
-                else:
-                    print('\x1b[1;32;40m' + 'Packet received' + '\x1b[0m')
-                    tam = len(pckg) - 4
-                    pckg = struct.unpack_from(f'i {tam}s', pckg)
-                    seq = pckg[0]
-                    payload = pckg[1:]
-
-                    if seq == 1:
-                        self.action = "sendAck1" # If the sequence of the packet is as expected, send sequence 1 ack, and should change the state
-                        if(payload[0] == b'END'): # If received final message from sender, end the loop
-                            self.endFlag = 1
-                        for i in payload:
-                            file.write(i)
-                    else: 
-                        self.action = "sendAck0" # If the sequence of the packet is not as expected, send sequence 0 ack, and should remain in the same state
-                        if(payload[0] == b'END'): # If received final message from sender, end the loop
-                            self.endFlag = 1
-
-            if self.action == "sendAck0":
-                print('\x1b[1;34;40m' + 'Ack 0 sent' + '\x1b[0m')
-                self.sendAck(0, 0, target_address) # Send sequence 0 ack
-                if self.endFlag:
-                    file.close() 
-                    break
-                self.state = "waitSeq_1" # Change state to waitSeq_1
-                
-            elif self.action == "sendAck1":
-                print('\x1b[1;34;40m' + 'Ack 1 sent' + '\x1b[0m')
-                self.sendAck(1, 1, target_address) # Send sequence 1 ack
-                if self.endFlag: 
-                    file.close()
-                    break
-                self.state = "waitSeq_0" # Change state to waitSeq_0
+        file.close() # Transmission ended
 
 def main():
     client = UDPClient()
