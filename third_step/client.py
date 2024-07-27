17f1c88b1f57d20061d0dcfe7acf814c6ca5ae39
shellyda.de.barbosa <shellydabarbosa@gmail.com>
2024-07-27 15:04:20 -0300

    fix: commands logic and printing correct logs

--- third_step/client.py
+++ third_step/client.py
@@ -1,93 +1,82 @@
 import socket as skt
 import struct
 from threading import Thread
-import env_props as env # Environment props
-
-MAX_BUFF_SIZE = env.MAX_BUFF_SIZE
 
 class Client:
     def __init__(self, host, port):
-        # Create a UDP socket and associate it with the server address
         self.client_socket = skt.socket(skt.AF_INET, skt.SOCK_DGRAM)
-        self.server_addr = (host, port)
+        self.server_address = (host, port)
         self.username = None
 
     def send_message(self, message):
-        # Send a message to the server
-        self.client_socket.sendto(message.encode(), self.server_addr)
+        self.client_socket.sendto(message.encode(), self.server_address)
 
     def receive_message(self):
-        # Receive a message from the server
         while True:
-            data, _ = self.client_socket.recvfrom(MAX_BUFF_SIZE)
+            data, _ = self.client_socket.recvfrom(1024)
             print(data.decode())
 
     def login(self, username):
-        # Perform user login
         self.username = username
         self.send_message(f"login {username}")
 
     def logout(self):
-        # Perform user logout
-        if self.username:
-            self.send_message(f"logout {self.username}")
-            self.username = None
+        self.send_message(f"logout {self.username}")
+        self.username = None
 
-    def create_accommodation(self, name, location, description):
-        # Create a new accommodation
-        self.send_message(f"create {name} {location} {description}")
+    def create_accommodation(self, name, location):
+        self.send_message(f"create {name} {location}")
 
     def list_my_accommodations(self):
-        # List client's accommodations
-        if self.username:
-            self.send_message(f"list:myacmd {self.username}")
+        self.send_message(f"list:myacmd {self.username}")
 
     def list_accommodations(self):
-        # List all available accommodations
         self.send_message("list:acmd")
 
     def list_my_reservations(self):
-        # List client's reservations
-        if self.username:
-            self.send_message(f"list:myrsv {self.username}")
+        self.send_message(f"list:myrsv {self.username}")
 
-    def book_accommodation(self, owner, name, location, day, room):
-        # Book an accommodation
-        self.send_message(f"book {owner} {name} {location} {day} {room}")
+    def book_accommodation(self, owner, acmd_id, day):
+        self.send_message(f"book {owner} {acmd_id} {day}")
 
-    def cancel_reservation(self, owner, name, location, day):
-        # Cancel a reservation
-        self.send_message(f"cancel {owner} {name} {location} {day}")
+    def cancel_reservation(self, owner, acmd_id, day):
+        self.send_message(f"cancel {owner} {acmd_id} {day}")
+
+    def show_help(self):
+        self.send_message("--help")
 
     def run(self):
-        # Start the client and wait for user commands
-        print("Accommodation client started. Please login to continue.")
         Thread(target=self.receive_message).start()
-
         while True:
-            command = input()
-            action = command.split()[0]
-            if action == "login":
-                self.login(command.split()[1])
-            elif action == "logout":
-                self.logout()
-            elif action == "create":
-                self.create_accommodation(command.split()[1], command.split()[2], command.split()[3])
-            elif action == "list:myacmd":
-                self.list_my_accommodations()
-            elif action == "list:acmd":
-                self.list_accommodations()
-            elif action == "list:myrsv":
-                self.list_my_reservations()
-            elif action == "book":
-                self.book_accommodation(command.split()[1], command.split()[2], command.split()[3], command.split()[4], command.split()[5])
-            elif action == "cancel":
-                self.cancel_reservation(command.split()[1], command.split()[2], command.split()[3], command.split()[4])
-            elif action == "--help":
-                self.send_message("--help")
+            if self.username is None:
+                command = input("Enter your username to login: ")
+                self.login(command)
             else:
-                print('This command is not valid, try again! \nIf needed you can type --help to get the commands list!')
+                command = input(f"{self.username}@client:~$ ")
+                if command.startswith("login"):
+                    print("You are already logged in.")
+                elif command.startswith("logout"):
+                    self.logout()
+                elif command.startswith("create"):
+                    _, name, location = command.split(maxsplit=3)
+                    self.create_accommodation(name, location)
+                elif command.startswith("list:myacmd"):
+                    self.list_my_accommodations()
+                elif command.startswith("list:acmd"):
+                    self.list_accommodations()
+                elif command.startswith("list:myrsv"):
+                    self.list_my_reservations()
+                elif command.startswith("book"):
+                    _, owner, acmd_id, day = command.split()
+                    self.book_accommodation(owner, acmd_id, day)
+                elif command.startswith("cancel"):
+                    _, owner, acmd_id, day = command.split()
+                    self.cancel_reservation(owner, acmd_id, day)
+                elif command.startswith("--help"):
+                    self.show_help()
+                else:
+                    self.send_message(command)
 
 if __name__ == "__main__":
-    client = Client(env.SERVER_HOST, env.SERVER_PORT)
+    client = Client('127.0.0.1', 12000)
     client.run()
--- third_step/server.py
+++ third_step/server.py
@@ -1,31 +1,26 @@
 import socket as skt
 import struct
 from threading import Thread
-import env_props as env # Environment props
-
-MAX_BUFF_SIZE = env.MAX_BUFF_SIZE
 
 class Server:
     def __init__(self, host, port):
-        # Create a UDP socket and bind it to the specified address
         self.server_socket = skt.socket(skt.AF_INET, skt.SOCK_DGRAM)
         self.server_socket.bind((host, port))
-        self.clients = {}  # Dictionary to store connected clients
-        self.accommodations = {}  # Dictionary to store accommodations
-        self.reservations = {}  # Dictionary to store reservations
+        self.clients = {}  # {username: (address, port)}
+        self.accommodations = {}  # {(name, location): {'id': id, 'owner': (username, addr), 'description': description, 'available_days': [days], 'reservations': {day: (username, addr)}}}
+        self.reservations = {}  # {(owner, id, day): (guest_name, guest_addr)}
+        self.available_days = ["17/07/2024", "18/07/2024", "19/07/2024", "20/07/2024", "21/07/2024", "22/07/2024"]
 
     def handle_client(self, data, addr):
-        # Process the received message from the client
         command = data.decode().split()
         action = command[0]
 
-        # Perform the action corresponding to the received command
         if action == "login":
             self.login(command[1], addr)
         elif action == "logout":
-            self.logout(command[1], addr)
+            self.logout(command[1])
         elif action == "create":
-            self.create_accommodation(command[1], command[2], command[3], addr)
+            self.create_accommodation(command[1], command[2], addr)
         elif action == "list:myacmd":
             self.list_my_accommodations(command[1], addr)
         elif action == "list:acmd":
@@ -33,118 +28,135 @@ class Server:
         elif action == "list:myrsv":
             self.list_my_reservations(command[1], addr)
         elif action == "book":
-            self.book_accommodation(command[1], command[2], command[3], command[4], command[5], addr)
+            self.book_accommodation(command[1], command[2], command[3], addr)
         elif action == "cancel":
-            self.cancel_reservation(command[1], command[2], command[3], command[4], addr)
+            self.cancel_reservation(command[1], command[2], command[3], addr)
         elif action == "--help":
             self.show_help(addr)
 
     def login(self, username, addr):
-        # Add the client to the connected clients list
         if username in self.clients:
             self.server_socket.sendto("Username already in use.".encode(), addr)
         else:
             self.clients[username] = addr
-            self.server_socket.sendto("You are online!".encode(), addr)
+            self.server_socket.sendto("You are online! Use --help to see available commands.".encode(), addr)
     
-    def logout(self, username, addr):
-        # Remove the client from the connected clients list
+    def logout(self, username):
         if username in self.clients:
             del self.clients[username]
-            self.server_socket.sendto("You are offline!".encode(), addr)
-        else:
-            self.server_socket.sendto("You need to be logged!".encode(), addr)
     
-    def create_accommodation(self, name, location, description, addr):
-        # Create a new accommodation
+    def create_accommodation(self, name, location, addr):
         if (name, location) in self.accommodations:
             self.server_socket.sendto("Accommodation already exists.".encode(), addr)
         else:
+            accommodation_id = len(self.accommodations) + 1
             self.accommodations[(name, location)] = {
-                'description': description,
+                'id': accommodation_id,
                 'owner': addr,
+                'available_days': self.available_days.copy(),
                 'reservations': {}
             }
-            self.notify_all_clients(f"Accommodation {name} in {location} created successfully!")
+            owner_name = self.get_username_by_address(addr)
+            self.server_socket.sendto(f"Accommodation {name} created successfully!".encode(), addr)
+            self.notify_all_clients(f"[{owner_name}/{addr[0]}:{addr[1]}] New accommodation {name} in {location} created!", exclude=addr)
     
     def list_my_accommodations(self, username, addr):
-        # List accommodations owned by the client
-        my_acmds = [f"{name} in {location}: {details['description']}" 
-                    for (name, location), details in self.accommodations.items() 
-                    if details['owner'] == addr]
+        my_acmds = []
+        for (name, location), details in self.accommodations.items():
+            if details['owner'] == addr:
+                res = details['reservations']
+                acmd_info = f"ID {details['id']}: {name} in {location} - Available: {details['available_days']} - Reserved: {[(d, self.get_username_by_address(u)) for d, u in res.items()]}"
+                my_acmds.append(acmd_info)
         response = "\n".join(my_acmds) if my_acmds else "You have no accommodations."
         self.server_socket.sendto(response.encode(), addr)
     
     def list_accommodations(self, addr):
-        # List all available accommodations
-        acmds = [f"{name} in {location}: {details['description']}" 
-                 for (name, location), details in self.accommodations.items()]
+        acmds = []
+        for (name, location), details in self.accommodations.items():
+            owner_name = self.get_username_by_address(details['owner'])
+            acmd_info = f"ID {details['id']}: {name} in {location} - Available: {details['available_days']} - Owner: {owner_name}"
+            acmds.append(acmd_info)
         response = "\n".join(acmds) if acmds else "No accommodations available."
         self.server_socket.sendto(response.encode(), addr)
     
     def list_my_reservations(self, username, addr):
-        # List reservations made by the client
-        my_rsvs = [f"{owner} {name} in {location} on {day}: Room {room}" 
-                   for (owner, name, location, day), room in self.reservations.items() 
-                   if room['guest'] == addr]
+        my_rsvs = []
+        for (owner, id, day), (guest_name, guest_addr) in self.reservations.items():
+            if guest_addr == addr:
+                owner_name = self.get_username_by_address(owner)
+                my_rsvs.append(f"[{owner_name}/{owner[0]}:{owner[1]}] Reservation: Accommodation ID {id} on {day}")
         response = "\n".join(my_rsvs) if my_rsvs else "You have no reservations."
         self.server_socket.sendto(response.encode(), addr)
     
-    def book_accommodation(self, owner, name, location, day, room, addr):
-        # Book an accommodation for the client
-        key = (owner, name, location, day)
-        if key in self.reservations:
+    def book_accommodation(self, owner_name, acmd_id, day, addr):
+        owner_addr = self.get_addr_by_username(owner_name)
+        if owner_addr and (owner_addr, acmd_id, day) in self.reservations:
             self.server_socket.sendto("Accommodation already booked for this day.".encode(), addr)
         else:
-            self.reservations[key] = {'room': room, 'guest': addr}
-            self.server_socket.sendto("Booking successful.".encode(), addr)
-            self.notify_owner(owner, f"Booking made by {addr} for {name} in {location} on {day}")
+            if (owner_addr, acmd_id, day) not in self.reservations:
+                for (name, location), details in self.accommodations.items():
+                    if details['id'] == int(acmd_id) and day in details['available_days']:
+                        details['available_days'].remove(day)
+                        details['reservations'][day] = addr
+                        self.reservations[(owner_addr, acmd_id, day)] = (self.get_username_by_address(addr), addr)
+                        self.server_socket.sendto(f"Booking successful for {name} on {day}.".encode(), addr)
+                        self.notify_owner(owner_addr, f"[{self.get_username_by_address(addr)}/{addr[0]}:{addr[1]}] Reservation for {name} on {day}", subject="reservation")
+                        return
+            self.server_socket.sendto("Accommodation or day unavailable.".encode(), addr)
     
-    def cancel_reservation(self, owner, name, location, day, addr):
-        # Cancel a reservation for the client
-        key = (owner, name, location, day)
-        if key in self.reservations and self.reservations[key]['guest'] == addr:
-            del self.reservations[key]
-            self.server_socket.sendto("Reservation cancelled successfully.".encode(), addr)
-            self.notify_owner(owner, f"Reservation cancelled by {addr} for {name} in {location} on {day}")
-        else:
-            self.server_socket.sendto("Reservation not found.".encode(), addr)
+    def cancel_reservation(self, owner_name, acmd_id, day, addr):
+        owner_addr = self.get_addr_by_username(owner_name)
+        if owner_addr and (owner_addr, acmd_id, day) in self.reservations and self.reservations[(owner_addr, acmd_id, day)][1] == addr:
+            del self.reservations[(owner_addr, acmd_id, day)]
+            for (name, location), details in self.accommodations.items():
+                if details['id'] == int(acmd_id):
+                    details['available_days'].append(day)
+                    del details['reservations'][day]
+                    self.server_socket.sendto("Reservation cancelled successfully.".encode(), addr)
+                    self.notify_owner(owner_addr, f"[{self.get_username_by_address(addr)}/{addr[0]}:{addr[1]}] Cancellation for {name} on {day}", subject="cancellation")
+                    self.notify_all_clients(f"[{self.get_username_by_address(owner_addr)}/{owner_addr[0]}:{owner_addr[1]}] New availability for {name} in {location} on {day}", exclude=owner_addr)
+                    return
+        self.server_socket.sendto("Reservation not found.".encode(), addr)
     
-    def notify_all_clients(self, message):
-        # Notify all connected clients
+    def notify_all_clients(self, message, exclude=None):
         for client_addr in self.clients.values():
-            self.server_socket.sendto(message.encode(), client_addr)
+            if client_addr != exclude:
+                self.server_socket.sendto(message.encode(), client_addr)
     
-    def notify_owner(self, owner, message):
-        # Notify the owner of an accommodation
-        owner_addr = self.clients.get(owner)
+    def notify_owner(self, owner_addr, message, subject):
         if owner_addr:
             self.server_socket.sendto(message.encode(), owner_addr)
-
+    
     def show_help(self, addr):
-        # Display a list of available commands
         help_message = """
 Available commands:
 - login <username>: Log in with a username
 - logout <username>: Log out
-- create <name> <location> <description>: Create a new accommodation
+- create <name> <location>: Create a new accommodation
 - list:myacmd <username>: List your accommodations
 - list:acmd: List all available accommodations
 - list:myrsv <username>: List your reservations
-- book <owner> <name> <location> <day> <room>: Book an accommodation
-- cancel <owner> <name> <location> <day>: Cancel a reservation
+- book <owner> <id> <day>: Book an accommodation
+- cancel <owner> <id> <day>: Cancel a reservation
 - --help: Display this help message
 """
         self.server_socket.sendto(help_message.encode(), addr)
+    
+    def get_username_by_address(self, addr):
+        for username, client_addr in self.clients.items():
+            if client_addr == addr:
+                return username
+        return None
 
+    def get_addr_by_username(self, username):
+        return self.clients.get(username, None)
 
     def run(self):
-        # Start the server and wait for connections
         print("Accommodation server started. Waiting for connections...")
         while True:
-            data, addr = self.server_socket.recvfrom(MAX_BUFF_SIZE)
+            data, addr = self.server_socket.recvfrom(1024)
             Thread(target=self.handle_client, args=(data, addr)).start()
 
 if __name__ == "__main__":
-    server = Server(env.SERVER_HOST, env.SERVER_PORT)
+    server = Server('127.0.0.1', 12000)
     server.run()
